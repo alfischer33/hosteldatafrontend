4c91086b7283954fad6f0c9a842c65dc6bd775d2
Austin Fischer <alfischer33@gmail.com>
2021-02-27 14:58:40 -0700

    updated secrets

---
+++ frontendpractice/secrets.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '@g$*mmw3_7ks0o%s7@8kxbpy#g*u$ow#rjwzo2-)m(ywon2!-2'
--- frontendpractice/settings.py
+++ frontendpractice/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@g$*mmw3_7ks0o%s7@8kxbpy#g*u$ow#rjwzo2-)m(ywon2!-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
